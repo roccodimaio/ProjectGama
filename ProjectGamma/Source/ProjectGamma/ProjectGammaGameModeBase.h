be67772e6ae9fe5b44fda6011f142e56085bc9c6
rdimaio <roccodimaio@yahoo.com>
2020-04-24 13:20:43 -0400

    Initial Commit

---
+++ ProjectGamma/Source/ProjectGamma/ProjectGammaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectGammaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGAMMA_API AProjectGammaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
