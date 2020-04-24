e754958fd6c679218f09c0c6f69fe962e2fd0871
rdimaio <roccodimaio@yahoo.com>
2020-04-24 15:55:16 -0400

    Updated Main to include turn and movement input and key bindings

---
+++ ProjectGamma/Source/ProjectGamma/Main.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Main.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/World.h"
+#include "Components/InputComponent.h"
+
+
+// Sets default values
+AMain::AMain()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	CameraBoomLength = 600.0f; 
+
+	// Create Camera Boom (pulls towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent()); // Attacheds the CameraBoom to the root component
+	CameraBoom->TargetArmLength = CameraBoomLength; // Camera follows at this distance
+	CameraBoom->bUsePawnControlRotation = true; // Rotate arm based on controller
+
+	// Create follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamers"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attached FollowCamera to the CameraBoom on socket SocketName
+	FollowCamera->bUsePawnControlRotation = false; // Camera will only follow camera boom, will not follow controller
+
+	// Set our turn rates for input
+	BaseTurnRate = 65.0f; 
+	BaseLookUpRate = 65.0f; 
+
+}
+
+// Called when the game starts or when spawned
+void AMain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMain::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	check(PlayerInputComponent);  
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump); // Inherited from Character class
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping); // Inherited from Character class
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMain::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMain::MoveRight);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput); // Inherited from Pawn class
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput); // Inherited from Pawn class
+
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMain::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AMain::LookUpAtRate);
+
+}
+
+void AMain::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation(); // Provides direction the controller is facing
+		const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f); //Assign the Yaw of Rotation to YawRotation
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); // Direction we are looking at. Pull X axis (forward) of the controller from the rotation matrix based on the Yaw rotation
+
+		AddMovementInput(Direction, Value); // Add movement input in the direction calculated above
+	}
+}
+
+void AMain::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// Find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation(); // Provides direction the controller is facing
+		const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f); //Assign the Yaw of Rotation to YawRotation
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y); // Direction we are looking at. Pull Y axis (right) of the controller from the rotation matrix based on the Yaw rotation
+
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMain::TurnAtRate(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds()); // Turn at: Rate (Between 0 and 1) * BaseTurnRate (65.0f) * time between frames (DeltaTime)
+}
+
+void AMain::LookUpAtRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds()); // Look up/down at: Rate (Between 0 and 1) * BaseLookUpAtRate (65.0f) * time between frames (DeltaTime)
+}
+
---
+++ ProjectGamma/Source/ProjectGamma/Main.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Main.generated.h"
+
+UCLASS()
+class PROJECTGAMMA_API AMain : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMain();
+
+	// Subobjects
+
+	/** Camera boom to position the camera behind the player */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera; 
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	float CameraBoomLength;
+	
+	/** Base turn rate to scale turning functions for the camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	float BaseTurnRate; 
+
+	/** Base look up rate to scale turning functions for the camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	float BaseLookUpRate;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Public Functions
+
+	/* Called for forwards/backwards input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/** Called via input to turn at a given rate 
+	* @param Rate This is a normalized rate i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+
+	/** Called via input to look up/down at a given rate
+	* @param Rate This is a normalized rate i.e. 1.0 means 100% of desired look up/down rate
+	*/
+	void LookUpAtRate(float Rate);
+
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
